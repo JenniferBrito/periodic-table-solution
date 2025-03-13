91a4b5f0e8fd14ed157ef8395331f6c36897c83e
Jennifer Brito <jennifer.brito3@gmail.com>
2025-03-13 16:19:50 +0000

    fix: deleting non existent element from properties and elements table

--- fix_database.sh
+++ fix_database.sh
@@ -9,16 +9,22 @@
 #  FIX_BOILING_POINT=$($PSQL "ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;")
 #  echo "$FIX_BOILING_POINT"
 
-ADD_NOT_NULL_MPC=$($PSQL "ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;")
- echo "$ADD_NOT_NULL_MPC"
- ADD_NOT_NULL_BPC=$($PSQL "ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;")
- echo "$ADD_NOT_NULL_BPC"
+# ADD_NOT_NULL_MPC=$($PSQL "ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;")
+#  echo "$ADD_NOT_NULL_MPC"
+#  ADD_NOT_NULL_BPC=$($PSQL "ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;")
+#  echo "$ADD_NOT_NULL_BPC"
  
- FIX_NAME=$($PSQL "ALTER TABLE elements ALTER COLUMN name SET NOT NULL;")
- echo "$FIX_NAME"
- UNIQUE_NAME=$($PSQL "ALTER TABLE elements ADD CONSTRAINT name UNIQUE(name);")
- echo "$UNIQUE_NAME"
- FIX_SYMBOL=$($PSQL "ALTER TABLE elements ALTER COLUMN symbol SET NOT NULL;")
- echo "$FIX_SYMBOL"
- UNIQUE_SYMBOL=$($PSQL "ALTER TABLE elements ADD CONSTRAINT symbol UNIQUE(symbol);")
- echo "$UNIQUE_SYMBOL"
+#  FIX_NAME=$($PSQL "ALTER TABLE elements ALTER COLUMN name SET NOT NULL;")
+#  echo "$FIX_NAME"
+#  UNIQUE_NAME=$($PSQL "ALTER TABLE elements ADD CONSTRAINT name UNIQUE(name);")
+#  echo "$UNIQUE_NAME"
+#  FIX_SYMBOL=$($PSQL "ALTER TABLE elements ALTER COLUMN symbol SET NOT NULL;")
+#  echo "$FIX_SYMBOL"
+#  UNIQUE_SYMBOL=$($PSQL "ALTER TABLE elements ADD CONSTRAINT symbol UNIQUE(symbol);")
+#  echo "$UNIQUE_SYMBOL"
+
+DELETE_FROM_PROPERTIES=$($PSQL "DELETE FROM properties WHERE atomic_number=1000;")
+echo "$DELETE_FROM_PROPERTIES"
+
+DELETE_FROM_ELEMENT=$($PSQL "DELETE FROM elements WHERE atomic_number=1000;")
+echo "$DELETE_FROM_ELEMENT"
